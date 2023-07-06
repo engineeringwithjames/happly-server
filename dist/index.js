497b1d5b66ba907ed3d7419a1f59cab3b8d3df80
James Odeyale <jamesodeyale01@gmail.com>
2023-07-06 17:17:28 -0400

    hello new project

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var port = 8000;
+var app = (0, express_1.default)();
+app.get("/", function (req, res) {
+    res.send("HELLO FROM EXPRESS + TS!!!!");
+});
+app.get("/hi", function (req, res) {
+    res.send("BYEEE!!");
+});
+app.listen(port, function () {
+    console.log("now listening on port ".concat(port));
+});
