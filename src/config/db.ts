b6a1c489d5b7bd710f045cae12f1a2fb72c7842d
James Odeyale <jamesodeyale01@gmail.com>
2023-08-27 22:25:23 -0400

    adding console log 5

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,12 +2,14 @@ import admin, { ServiceAccount } from "firebase-admin";
 
 require("dotenv").config();
 
+const SERVICE_ACCOUNT_PRIVATE_KEY = process.env.SERVICE_ACCOUNT_PRIVATE_KEY
+  ? JSON.parse(process.env.SERVICE_ACCOUNT_PRIVATE_KEY)
+  : undefined;
+
 const serviceAccount: ServiceAccount = {
   projectId: process.env.SERVICE_ACCOUNT_PROJECT_ID,
   clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-  privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY
-    ? process.env.SERVICE_ACCOUNT_PRIVATE_KEY
-    : undefined
+  privateKey: SERVICE_ACCOUNT_PRIVATE_KEY.privateKey
 };
 
 console.log(serviceAccount);
