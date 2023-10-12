f0bb5b89ef468988dbe3fd991d3b5ebd3772e052
James Odeyale <jamesodeyale01@gmail.com>
2023-10-11 21:51:38 -0400

    migrated to gcp cloud thanks to jenis

--- dist/config/db.js
+++ dist/config/db.js
@@ -6,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
 require("dotenv").config();
-const SERVICE_ACCOUNT_PRIVATE_KEY = process.env.SERVICE_ACCOUNT_PRIVATE_KEY
-    ? JSON.parse(process.env.SERVICE_ACCOUNT_PRIVATE_KEY)
-    : undefined;
+// const SERVICE_ACCOUNT_PRIVATE_KEY = process.env.SERVICE_ACCOUNT_PRIVATE_KEY
+//   ? JSON.parse(process.env.SERVICE_ACCOUNT_PRIVATE_KEY)
+//   : undefined;
 const serviceAccount = {
     projectId: process.env.SERVICE_ACCOUNT_PROJECT_ID,
     clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-    privateKey: SERVICE_ACCOUNT_PRIVATE_KEY.privateKey
+    privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY
 };
 // Initialize Firebase Admin SDK
 firebase_admin_1.default.initializeApp({
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,14 +2,10 @@ import admin, { ServiceAccount } from "firebase-admin";
 
 require("dotenv").config();
 
-const SERVICE_ACCOUNT_PRIVATE_KEY = process.env.SERVICE_ACCOUNT_PRIVATE_KEY
-  ? JSON.parse(process.env.SERVICE_ACCOUNT_PRIVATE_KEY)
-  : undefined;
-
 const serviceAccount: ServiceAccount = {
   projectId: process.env.SERVICE_ACCOUNT_PROJECT_ID,
   clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-  privateKey: SERVICE_ACCOUNT_PRIVATE_KEY.privateKey
+  privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY
 };
 
 // Initialize Firebase Admin SDK
